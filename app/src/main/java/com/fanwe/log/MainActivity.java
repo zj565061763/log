a4619c755bb82ccf17bfff37dc33bbb2b2cab743
zjun <565061763@qq.com>
2018-01-15 11:27:02 +0800

    重构

--- app/src/main/java/com/fanwe/log/MainActivity.java
+++ app/src/main/java/com/fanwe/log/MainActivity.java
@@ -3,6 +3,7 @@ package com.fanwe.log;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.fanwe.lib.log.FLogFormatter;
 import com.fanwe.lib.log.FLogger;
 
 import java.io.File;
@@ -10,7 +11,6 @@ import java.io.IOException;
 import java.util.logging.FileHandler;
 import java.util.logging.Handler;
 import java.util.logging.Level;
-import java.util.logging.SimpleFormatter;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -28,7 +28,7 @@ public class MainActivity extends AppCompatActivity
         try
         {
             Handler handler = new FileHandler(path, limit, count, true);
-            handler.setFormatter(new SimpleFormatter());
+            handler.setFormatter(new FLogFormatter());
             FLogger.get().addHandler(handler);
         } catch (IOException e)
         {
@@ -36,6 +36,18 @@ public class MainActivity extends AppCompatActivity
         }
 
         FLogger.get().log(Level.INFO, "onCreate");
+
+        String test = null;
+        if (true)
+        {
+            try
+            {
+                test.equals("");
+            } catch (Exception e)
+            {
+                FLogger.get().log(Level.SEVERE, e.toString(), e);
+            }
+        }
     }
 
     @Override
---
+++ lib/src/main/java/com/fanwe/lib/log/FLogFormatter.java
@@ -0,0 +1,59 @@
+package com.fanwe.lib.log;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.Formatter;
+import java.util.logging.LogRecord;
+
+/**
+ * Created by Administrator on 2018/1/15.
+ */
+public class FLogFormatter extends Formatter
+{
+    private final Date mDate = new Date();
+    private final SimpleDateFormat mDateFormat = new SimpleDateFormat("yyyyMMdd HH:mm:ss.SSS");
+    private StringBuilder mStringBuilder = new StringBuilder();
+
+    private void clearStringBuilder()
+    {
+        if (mStringBuilder.length() > 0)
+        {
+            mStringBuilder.delete(0, mStringBuilder.length());
+        }
+    }
+
+    @Override
+    public String format(LogRecord record)
+    {
+        clearStringBuilder();
+
+        mDate.setTime(record.getMillis());
+        String date = mDateFormat.format(mDate);
+
+        String message = formatMessage(record);
+
+        String throwable = "";
+        if (record.getThrown() != null)
+        {
+            StringWriter sw = new StringWriter();
+            PrintWriter pw = new PrintWriter(sw);
+            pw.println();
+            record.getThrown().printStackTrace(pw);
+            pw.close();
+            throwable = sw.toString();
+        }
+
+        mStringBuilder.append(date).append("(").append(record.getLevel()).append(") ").append(message);
+        mStringBuilder.append(throwable);
+
+        mStringBuilder.append(getNextLine());
+        return mStringBuilder.toString();
+    }
+
+    private static String getNextLine()
+    {
+        return System.getProperty("line.separator");
+    }
+}
