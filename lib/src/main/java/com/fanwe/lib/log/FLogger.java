48dfe8d9debaaa631ed09cfa17f4ef9149a11d92
zjun <565061763@qq.com>
2018-01-15 15:52:36 +0800

    1.0.3

--- lib/src/main/java/com/fanwe/lib/log/FLogger.java
+++ lib/src/main/java/com/fanwe/lib/log/FLogger.java
@@ -2,8 +2,12 @@ package com.fanwe.lib.log;
 
 import android.text.TextUtils;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
+import java.util.logging.Handler;
 import java.util.logging.Logger;
 
 public class FLogger
@@ -48,5 +52,41 @@ public class FLogger
             return;
         }
         MAP_LOGGER.remove(name);
+    }
+
+    public final static List<Handler> getHandlers(Logger logger)
+    {
+        if (logger == null)
+        {
+            return null;
+        }
+        Handler[] handlers = logger.getHandlers();
+        if (handlers == null || handlers.length <= 0)
+        {
+            return null;
+        }
+        return Arrays.asList(handlers);
+    }
+
+    public final static void removeHandler(Class<?> clazz, Logger logger)
+    {
+        if (clazz == null)
+        {
+            return;
+        }
+        List<Handler> listHandler = getHandlers(logger);
+        if (listHandler == null)
+        {
+            return;
+        }
+        Iterator<Handler> it = listHandler.iterator();
+        while (it.hasNext())
+        {
+            Handler item = it.next();
+            if (item.getClass() == clazz)
+            {
+                logger.removeHandler(item);
+            }
+        }
     }
 }
