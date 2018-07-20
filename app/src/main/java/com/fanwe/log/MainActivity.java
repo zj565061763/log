52df3d715465fd9785327c8962208decf53f6c27
zjun <565061763@qq.com>
2018-07-20 17:20:34 +0800

    refactor

--- app/src/main/java/com/fanwe/log/MainActivity.java
+++ app/src/main/java/com/fanwe/log/MainActivity.java
@@ -3,14 +3,9 @@ package com.fanwe.log;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import com.fanwe.lib.log.FFileHandler;
 import com.fanwe.lib.log.FLogger;
 import com.fanwe.lib.looper.FLooper;
 import com.fanwe.lib.looper.impl.FSimpleLooper;
-
-import java.io.IOException;
-import java.util.logging.Handler;
-import java.util.logging.Level;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -23,23 +18,16 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        try
-        {
-            Handler handler = new FFileHandler("testlog", 1 * FFileHandler.MB, this);
-            FLogger.get().addHandler(handler);
-        } catch (IOException e)
-        {
-            e.printStackTrace();
-        }
+        FLogger.get().setLogFileEnable(this);
 
-        mLooper.setInterval(10);
+        mLooper.setInterval(1000);
         mLooper.start(new Runnable()
         {
             @Override
             public void run()
             {
                 mCount++;
-                FLogger.get().log(Level.INFO, "loop count:" + mCount);
+                FLogger.get().info("loop count:" + mCount);
             }
         });
     }
@@ -48,7 +36,7 @@ public class MainActivity extends AppCompatActivity
     protected void onDestroy()
     {
         super.onDestroy();
-        FLogger.get().log(Level.INFO, "onDestroy");
+        FLogger.get().info("onDestroy");
         mLooper.stop();
     }
 }
--- lib/src/main/java/com/fanwe/lib/log/FLogger.java
+++ lib/src/main/java/com/fanwe/lib/log/FLogger.java
@@ -1,28 +1,35 @@
 package com.fanwe.lib.log;
 
+import android.content.Context;
 import android.text.TextUtils;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.logging.Handler;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class FLogger
 {
-    private static final Map<String, Logger> MAP_LOGGER = new HashMap<>();
+    private static final Map<String, FLogger> MAP_LOGGER = new HashMap<>();
+    private final Logger mLogger;
+    private Handler mDefaultHandler;
 
-    protected FLogger()
+    protected FLogger(Logger logger)
     {
+        if (logger == null)
+            throw new NullPointerException("logger is null");
+        mLogger = logger;
     }
 
     /**
-     * 返回默认的Logger对象(名字为FLogger)
+     * 返回默认的对象(名字为FLogger)
      *
      * @return
      */
-    public static Logger get()
+    public static FLogger get()
     {
-        return get(FLogger.class.getName());
+        return get(FLogger.class.getSimpleName());
     }
 
     /**
@@ -31,44 +38,105 @@ public class FLogger
      * @param name
      * @return
      */
-    public final synchronized static Logger get(String name)
+    public final static FLogger get(String name)
     {
         if (TextUtils.isEmpty(name))
             return null;
 
-        Logger logger = MAP_LOGGER.get(name);
-        if (logger == null)
+        synchronized (MAP_LOGGER)
         {
-            logger = Logger.getLogger(name);
-            MAP_LOGGER.put(name, logger);
+            FLogger logger = MAP_LOGGER.get(name);
+            if (logger == null)
+            {
+                logger = new FLogger(Logger.getLogger(name));
+                MAP_LOGGER.put(name, logger);
+            }
+            return logger;
         }
-        return logger;
+    }
+
+    public final String getName()
+    {
+        return mLogger.getName();
+    }
+
+    private Handler getDefaultHandler(Context context)
+    {
+        if (mDefaultHandler == null)
+        {
+            try
+            {
+                mDefaultHandler = new FFileHandler(getName() + ".log", 100 * FFileHandler.MB, context);
+            } catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
+        return mDefaultHandler;
     }
 
     /**
-     * 移除Logger对象
+     * 设置是否打开日志缓存到文件的功能
      *
-     * @param logger
+     * @param context null-关闭，不为null-打开
      */
-    public final synchronized static void remove(Logger logger)
+    public final void setLogFileEnable(Context context)
     {
-        if (logger == null)
-            return;
-
-        MAP_LOGGER.remove(logger.getName());
+        removeHandlers(mLogger);
+        if (context != null)
+            mLogger.addHandler(getDefaultHandler(context));
     }
+
+    /**
+     * 设置日志等级
+     *
+     * @param level
+     */
+    public final void setLevel(Level level)
+    {
+        mLogger.setLevel(level);
+    }
+
+    /**
+     * 移除当前对象
+     */
+    public final void remove()
+    {
+        synchronized (MAP_LOGGER)
+        {
+            MAP_LOGGER.remove(this);
+        }
+    }
+
+    //---------- log start ----------
+
+    public final void log(Level level, String msg)
+    {
+        mLogger.log(level, msg);
+    }
+
+    public final void info(String msg)
+    {
+        mLogger.info(msg);
+    }
+
+    public final void warning(String msg)
+    {
+        mLogger.warning(msg);
+    }
+
+    public final void severe(String msg)
+    {
+        mLogger.severe(msg);
+    }
+
+    //---------- log end ----------
 
     //---------- utils start ----------
 
-    public final static void removeHandler(Class<?> clazz, Logger logger)
+    private final static void removeHandlers(Logger logger)
     {
-        if (clazz == null || logger == null)
-            return;
-
         final Handler[] handlers = logger.getHandlers();
-        if (handlers == null || handlers.length <= 0)
-            return;
-
         for (Handler item : handlers)
         {
             logger.removeHandler(item);
