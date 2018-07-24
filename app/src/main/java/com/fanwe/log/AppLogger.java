3ca3f296e06469f7faa439214fd1835437855fed
zjun <565061763@qq.com>
2018-07-24 16:48:10 +0800

    demo

--- app/src/main/java/com/fanwe/log/AppLogger.java
+++ app/src/main/java/com/fanwe/log/AppLogger.java
@@ -1,12 +1,16 @@
 package com.fanwe.log;
 
+import android.util.Log;
+
 import com.fanwe.lib.log.FLogger;
 
 public class AppLogger extends FLogger
 {
+    public static final String TAG = AppLogger.class.getSimpleName();
+
     @Override
     protected void onCreate()
     {
-
+        Log.i(TAG, "onCreate:" + this);
     }
 }
