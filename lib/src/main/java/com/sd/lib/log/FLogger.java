59c7f6c2c3d69081c8190447c4762abc2e61b487
zjun <565061763@qq.com>
2019-05-30 14:59:26 +0800

    releaseIfNeed

--- lib/src/main/java/com/sd/lib/log/FLogger.java
+++ lib/src/main/java/com/sd/lib/log/FLogger.java
@@ -91,7 +91,7 @@ public abstract class FLogger
         {
             final Reference<? extends FLogger> reference = REFERENCE_QUEUE.poll();
             if (reference == null)
-                break;
+                return;
 
             for (Map.Entry<Class<?>, WeakReference<FLogger>> item : MAP_LOGGER.entrySet())
             {
