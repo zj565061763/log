538c8cf9671fa607f8724ed881030614c068b165
zjun <565061763@qq.com>
2020-10-16 09:24:31 +0800

    androidx

--- app/src/main/java/com/sd/log/MainActivity.java
+++ app/src/main/java/com/sd/log/MainActivity.java
@@ -1,7 +1,7 @@
 package com.sd.log;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.sd.lib.log.FLogger;
 
