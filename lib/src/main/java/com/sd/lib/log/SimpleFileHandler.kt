0c8b248186d8af3593301b669ca0fb3599a0e7f9
zhengjun <565061763@qq.com>
2022-06-16 01:33:58 +0800

    SimpleFileHandler

--- lib/src/main/java/com/sd/lib/log/SimpleFileHandler.kt
+++ lib/src/main/java/com/sd/lib/log/SimpleFileHandler.kt
@@ -1,51 +1,50 @@
-package com.sd.lib.log;
+package com.sd.lib.log
 
-import android.content.Context;
+import android.content.Context
+import java.io.File
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.logging.FileHandler
 
-import java.io.File;
-import java.io.IOException;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.logging.FileHandler;
+internal class SimpleFileHandler(
+    context: Context,
+    filename: String,
+    limitMB: Int,
+) : FileHandler(
+    getLogFilePath(context, filename + FILE_SUFFIX),
+    limitMB * MB,
+    1,
+    true,
+) {
+    companion object {
+        private const val MB = 1024 * 1024
+        private const val DIR_NAME = "flog"
+        private const val FILE_SUFFIX = ".log"
 
-class SimpleFileHandler extends FileHandler {
-    private static final int MB = 1024 * 1024;
-    private static final String DIR_NAME = "flog";
-    private static final String FILE_SUFFIX = ".log";
-
-    /**
-     * @param filename log文件名
-     * @param limitMB  log文件大小限制(单位MB)
-     */
-    public SimpleFileHandler(Context context, String filename, int limitMB) throws IOException, SecurityException {
-        super(getLogFilePath(context, filename + FILE_SUFFIX), limitMB * MB, 1, true);
-    }
-
-    private static String getLogFilePath(Context context, String fileName) {
-        final String today = newDateFormat().format(new Date());
-        final File dir = new File(getLogFileDir(context), today);
-        checkDir(dir);
-        return dir.getAbsolutePath() + File.separator + fileName;
-    }
-
-    static File getLogFileDir(Context context) {
-        File dir = context.getExternalFilesDir(DIR_NAME);
-        if (checkDir(dir)) {
-            return dir;
+        private fun getLogFilePath(context: Context, fileName: String): String {
+            val today = newDateFormat().format(Date())
+            val dir = File(getLogFileDir(context), today)
+            checkDir(dir)
+            return dir.absolutePath + File.separator + fileName
         }
 
-        dir = new File(context.getFilesDir(), DIR_NAME);
-        checkDir(dir);
-        return dir;
-    }
+        @JvmStatic
+        fun getLogFileDir(context: Context): File {
+            var dir = context.getExternalFilesDir(DIR_NAME)
+            if (checkDir(dir)) return dir!!
+            dir = File(context.filesDir, DIR_NAME)
+            checkDir(dir)
+            return dir
+        }
 
-    static DateFormat newDateFormat() {
-        return new SimpleDateFormat("yyyyMMdd");
-    }
+        @JvmStatic
+        fun newDateFormat(): DateFormat {
+            return SimpleDateFormat("yyyyMMdd")
+        }
 
-    private static boolean checkDir(File dir) {
-        if (dir == null) return false;
-        return dir.exists() || dir.mkdirs();
+        private fun checkDir(dir: File?): Boolean {
+            return if (dir == null) false else dir.exists() || dir.mkdirs()
+        }
     }
 }
