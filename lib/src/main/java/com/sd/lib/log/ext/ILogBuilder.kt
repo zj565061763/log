bff0a7714e589452de4ca753921604fa7e7ee0be
zhengjun <565061763@qq.com>
2022-06-16 03:30:30 +0800

    ILogBuilder

--- lib/src/main/java/com/sd/lib/log/ext/ILogBuilder.kt
+++ lib/src/main/java/com/sd/lib/log/ext/ILogBuilder.kt
@@ -1,36 +1,87 @@
 package com.sd.lib.log.ext
 
+import android.view.View
+
 interface ILogBuilder {
+    /**
+     * 设置格式化对象
+     */
     fun setFormatter(formatter: ILogFormatter?): ILogBuilder
 
+    /**
+     * 调用[pair]时，如果传入的value是[View]，是否用hash的方式显示该对象，默认true
+     */
     fun setHashPairView(hashView: Boolean): ILogBuilder
 
+    /**
+     * 添加一段内容，如果[content]为null或者空，则不添加
+     */
     fun add(content: Any?): ILogBuilder
 
+    /**
+     * 添加一对内容，如果[key]为null或者空，则不添加
+     */
     fun pair(key: String?, value: Any?): ILogBuilder
 
+    /**
+     * 用hash的方式显示[value]
+     */
     fun pairHash(key: String?, value: Any?): ILogBuilder
 
+    /**
+     * 用[Object.toString]的方式显示[value]
+     */
     fun pairStr(key: String?, value: Any?): ILogBuilder
 
+    /**
+     * 等价与：pair("instance", instance)，用hash的方式显示instance对象
+     */
     fun instance(instance: Any?): ILogBuilder
 
+    /**
+     * 等价与：pair("instance", instance)，用[Object.toString]的方式显示instance对象
+     */
     fun instanceStr(instance: Any?): ILogBuilder
 
+    /**
+     * 等价与：pair("uuid", uuid)
+     */
     fun uuid(uuid: String?): ILogBuilder
 
+    /**
+     * 换行
+     */
     fun nextLine(): ILogBuilder
 
+    /**
+     * 等价与：add(clazz.getSimpleName())
+     */
     fun clazz(clazz: Class<*>?): ILogBuilder
 
+    /**
+     * 等价与：add(clazz.getName())
+     */
     fun clazzFull(clazz: Class<*>?): ILogBuilder
 
+    /**
+     * 清空
+     */
     fun clear(): ILogBuilder
 
+    /**
+     * 构建字符串
+     */
     fun build(): String
 
     interface ILogFormatter {
+        /**
+         * 返回key-value之间的分隔符，默认：":"
+         */
         val separatorForKeyValue: String?
+
+        /**
+         * 返回段与段之间的分隔符，默认："|"
+         */
         val separatorBetweenPart: String?
     }
 }
