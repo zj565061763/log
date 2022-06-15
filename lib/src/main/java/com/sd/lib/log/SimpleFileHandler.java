d59ff1065ac3cd656ed58ac86b774b76d3b5d522
zhengjun <565061763@qq.com>
2022-06-15 22:33:04 +0800

    SimpleFileHandler

--- lib/src/main/java/com/sd/lib/log/SimpleFileHandler.java
+++ lib/src/main/java/com/sd/lib/log/SimpleFileHandler.java
@@ -13,21 +13,12 @@ class SimpleFileHandler extends FileHandler {
     private static final String DIR_NAME = "flog";
     private static final String FILE_SUFFIX = ".log";
 
-    private final Context mContext;
-    private final String mFilename;
-
     /**
-     * @param context
      * @param filename log文件名
      * @param limitMB  log文件大小限制(单位MB)
-     * @throws IOException
-     * @throws SecurityException
      */
     public SimpleFileHandler(Context context, String filename, int limitMB) throws IOException, SecurityException {
         super(getLogFilePath(context, filename + FILE_SUFFIX), limitMB * MB, 1, true);
-
-        mFilename = filename + FILE_SUFFIX;
-        mContext = context.getApplicationContext();
     }
 
     private static String getLogFilePath(Context context, String fileName) {
@@ -71,9 +62,7 @@ class SimpleFileHandler extends FileHandler {
     }
 
     private static boolean checkDir(File dir) {
-        if (dir == null) {
-            return false;
-        }
+        if (dir == null) return false;
         return dir.exists() || dir.mkdirs();
     }
 }
