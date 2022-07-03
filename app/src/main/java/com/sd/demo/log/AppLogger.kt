29bf081b9413d4d7d39191c8cd9097212f4e6575
zhengjun <565061763@qq.com>
2022-07-03 17:38:15 +0800

    log

--- app/src/main/java/com/sd/demo/log/AppLogger.kt
+++ app/src/main/java/com/sd/demo/log/AppLogger.kt
@@ -1,10 +1,16 @@
 package com.sd.demo.log
 
+import android.util.Log
 import com.sd.lib.log.FLogger
 
 class AppLogger : FLogger() {
     override fun onCreate() {
+        Log.i("AppLogger", "logger onCreate")
         // 开启日志文件，限制最大50MB
         openLogFile(50)
+    }
+
+    fun finalize() {
+        Log.i("AppLogger", "logger finalize")
     }
 }
