25a8c7f8be0f6b5437f6a5a8591372c9b814ab51
565061763 <565061763@qq.com>
2022-09-27 23:51:32 +0800

    SimpleFileHandler

--- lib/src/main/java/com/sd/lib/log/SimpleFileHandler.kt
+++ lib/src/main/java/com/sd/lib/log/SimpleFileHandler.kt
@@ -2,10 +2,15 @@ package com.sd.lib.log
 
 import android.content.Context
 import java.io.File
+import java.io.PrintWriter
+import java.io.StringWriter
 import java.text.DateFormat
 import java.text.SimpleDateFormat
 import java.util.*
 import java.util.logging.FileHandler
+import java.util.logging.Formatter
+import java.util.logging.Level
+import java.util.logging.LogRecord
 
 internal class SimpleFileHandler(
     context: Context,
@@ -68,5 +73,51 @@ internal class SimpleFileHandler(
 
     protected fun finalize() {
         close()
+    }
+}
+
+private class SimpleLogFormatter : Formatter() {
+    private val _dateFormat = SimpleDateFormat("MMdd HH:mm:ss.SSS")
+
+    override fun format(record: LogRecord): String {
+        val date = _dateFormat.format(Date(record.millis))
+        val message = formatMessage(record)
+
+        val thrown = record.thrown
+        val error = if (thrown == null) "" else {
+            val stringWriter = StringWriter()
+            PrintWriter(stringWriter).use {
+                it.println()
+                thrown.printStackTrace(it)
+            }
+            stringWriter.toString()
+        }
+
+        return buildString {
+            // 日期
+            append(date)
+            // 日志等级
+            append(getLevelString(record.level))
+            // 日志信息
+            append(message)
+            // 异常信息
+            append(error)
+            // 换行
+            append(nextLine)
+        }
+    }
+
+    companion object {
+        private val nextLine: String
+            get() = System.getProperty("line.separator") ?: "\r\n"
+
+        private fun getLevelString(level: Level): String {
+            return when (level) {
+                Level.INFO -> " "
+                Level.WARNING -> "(w)"
+                Level.SEVERE -> "(s)"
+                else -> "($level)"
+            }
+        }
     }
 }
