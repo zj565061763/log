99f62b1fa707df03784de7cbf7986fc827d1685e
zjun <565061763@qq.com>
2023-11-20 14:10:18 +0800

    demo

--- app/src/main/java/com/sd/demo/log/App.kt
+++ app/src/main/java/com/sd/demo/log/App.kt
@@ -3,7 +3,6 @@ package com.sd.demo.log
 import android.app.Application
 import com.sd.lib.log.FLogLevel
 import com.sd.lib.log.FLogger
-import com.sd.lib.log.flog
 
 class App : Application() {
     override fun onCreate() {
